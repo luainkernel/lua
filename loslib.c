luainkernel/lunatik

Fix redefinition of time_t bug

lua/loslib.c
@@ -328,8 +328,8 @@ static int os_date (lua_State *L) {
 
 
 static int os_time (lua_State *L) {
-  time_t t;
 #ifndef _KERNEL
+  time_t t;
   if (lua_isnoneornil(L, 1))  /* called without args? */
     t = time(NULL);  /* get current time */
   else {
@@ -351,6 +351,7 @@ static int os_time (lua_State *L) {
   l_pushtime(L, t);
   return 1;
 #else
+  lunatik_time t;
   lua_Integer res;
 
   luai_time(t);

lua/luaconf.h
@@ -842,18 +842,18 @@ extern void longjmp(luai_jmpbuf);
 #include <linux/ktime.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,6,0)
 #include <linux/time.h>
-typedef struct timespec time_t;
+typedef struct timespec lunatik_time;
 #define luai_time(t)		getnstimeofday(&t)
 #elif defined(LUA_32BITS)
 #error "kernel versions >= 5.6.0 do not support 32-bit time values"
 #else
-typedef struct timespec64 time_t;
+typedef struct timespec64 lunatik_time;
 #define luai_time(t)		ktime_get_real_ts64(&t)
 #endif
 
 static inline int time(void *p)
 {
-  time_t t;
+  lunatik_time t;
   ((void) p);
   luai_time(t);
   return t.tv_sec;

luainkernel/lua

loslib.c
@@ -328,8 +328,8 @@ static int os_date (lua_State *L) {
 
 
 static int os_time (lua_State *L) {
-  time_t t;
 #ifndef _KERNEL
+  time_t t;
   if (lua_isnoneornil(L, 1))  /* called without args? */
     t = time(NULL);  /* get current time */
   else {
@@ -351,6 +351,7 @@ static int os_time (lua_State *L) {
   l_pushtime(L, t);
   return 1;
 #else
+  lunatik_time_t t;
   lua_Integer res;
 
   luai_time(t);

luaconf.h
@@ -842,18 +842,18 @@ extern void longjmp(luai_jmpbuf);
 #include <linux/ktime.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5,6,0)
 #include <linux/time.h>
-typedef struct timespec time_t;
+typedef struct timespec lunatik_time_t;
 #define luai_time(t)		getnstimeofday(&t)
 #elif defined(LUA_32BITS)
 #error "kernel versions >= 5.6.0 do not support 32-bit time values"
 #else
-typedef struct timespec64 time_t;
+typedef struct timespec64 lunatik_time_t;
 #define luai_time(t)		ktime_get_real_ts64(&t)
 #endif
 
 static inline int time(void *p)
 {
-  time_t t;
+  lunatik_time_t t;
   ((void) p);
   luai_time(t);
   return t.tv_sec;